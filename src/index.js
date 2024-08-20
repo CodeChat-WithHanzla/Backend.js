4bfa35be992c197e760943d43ece7d11fe99de46
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-08-20 05:33:06 +0500

    utils asyncHandler ApiError and response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
 
 dotenv.config({
-  path:'./env'
-})
+  path: "./env",
+});
+
+const app = express();
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Listening at port : ${process.env.PORT}`);
+    });
+    app.on("error",(error)=>{
+    console.log("Error :: ",error);
+    
+    })
+  })
+  .catch(() => {});
