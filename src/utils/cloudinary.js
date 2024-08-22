bb7828893e1ce5340bf4fd96f592191f231a39ec
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-08-22 10:37:17 +0500

    Now our setup is configure and we have learn some filehandling , at last but not least i have write a sampleenv file

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+try {
+  if(!localFilePath) return null;
+  // upload on Cloudinary 
+  const response = await cloudinary.uploader.upload(localFilePath,{
+    resource_type: "auto"
+  })
+  // File has been uploaded successfully !
+  console.log("File has been uploaded successfully !",response.url);
+  return response;
+} catch (error) {
+  // remove the locally saved temporary file as upload operation goes failed !
+  fs.unlinkSync(localFilePath) 
+  return null;
+}
+}
+
+
+export {uploadOnCloudinary}
