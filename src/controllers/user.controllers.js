f659e021dc368da054f715711259b8f441dd0a96
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-08-31 20:33:07 +0500

    Login , logout , auth Middleware and user Routes modified

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,10 +4,29 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something wentwrong while generating :: generateAccessAndRefreshToken",
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
 
-  if ([fullName, email, username, password].some(field => field?.trim() === "")) {
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
     throw new ApiError(400, "All fields are required");
   }
 
@@ -23,13 +42,15 @@ const registerUser = asyncHandler(async (req, res) => {
 
   const avatarLocalPath = req.files?.avatar?.[0]?.path;
   const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
-  
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverImage = coverImageLocalPath ? await uploadOnCloudinary(coverImageLocalPath) : null;
+  const coverImage = coverImageLocalPath
+    ? await uploadOnCloudinary(coverImageLocalPath)
+    : null;
 
   if (!avatar) {
     throw new ApiError(400, "Error uploading avatar");
@@ -44,12 +65,76 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findById(user._id).select("-password -refreshToken");
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
 
   if (!createdUser) {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
 
-  return res.status(201).json(new ApiResponse(200, createdUser, "User registered Successfully"));
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // Accessing data from req.body
+  const { username, email, password } = req.body;
+
+  if (!email || !username)
+    throw new ApiError(404, "Email and password required ");
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exists ");
+  }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials ");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id,
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("AccessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      200,
+      {
+        user: loggedInUser,
+        accessToken,
+        refreshToken,
+      },
+      "User login Successfully!! ",
+    );
+});
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    },
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(200,{},"User logged out")
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,25 @@
-import {Router } from "express";
-import {registerUser} from '../controllers/user.controllers.js'
-import {upload} from '../middlewares/multer.middlewares.js'
+import { Router } from "express";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import {verifyJWT} from '../middlewares/auth.middlewares'
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(upload.fields([
+router.route("/register").post(
+  upload.fields([
     {
-        name : "avatar",
-        maxCount:1
-    },{
-        name : "coverImage",
-        maxCount: 1
-    }
-]),registerUser)
-export default router
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser,
+);
+router.route("/login").post(loginUser);
+// secured routes 
+router.route("logout").post(verifyJWT,logoutUser)
+
+export default router;
