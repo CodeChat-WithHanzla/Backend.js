843c67420926f6d06b806ccad7135a600abc423a
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-05 11:56:32 +0500

    Solve a bug in verifyJWT i was actually writing "accesstoken" which should be "AccessToken"

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -5,9 +5,9 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        const token = req.cookies?.AccessToken || req.header("Authorization")?.replace("Bearer ", "")
         
-        // console.log("Extracted :: ",token);
+        console.log("Request cookies :: ",req.cookies.AccessToken);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
