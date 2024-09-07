2daff79b931df146acb3ce9405e11d1048323e43
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-07 20:42:54 +0500

    Change Current Password

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -177,4 +177,15 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?.id);
+  const isPasswordCorrect = user.isPasswordCorrect(oldPassword);
+  if (!isPasswordCorrect) throw new ApiError(400, "Invalid Password! ");
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Change Successfully! "));
+});
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
